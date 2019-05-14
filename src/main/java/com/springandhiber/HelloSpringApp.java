9b303926b9a697b9ac9f3bb227a7646ef395727d
Sintaksis13 <jandos130@mail.ru>
2019-05-14 16:29:24 +0700

    Add CricketCoach class and setter injection for it

--- src/main/java/com/springandhiber/HelloSpringApp.java
+++ src/main/java/com/springandhiber/HelloSpringApp.java
@@ -7,9 +7,9 @@ public class HelloSpringApp {
         //initialize application context
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
         //retrieve bean from the spring container
-        Coach coach = context.getBean("myCoach", Coach.class);
+        Coach coach = context.getBean("myCricketCoach", Coach.class);
         //call bean methods
-        System.out.println(coach.getDailyWorkout());
+        System.out.println(coach.getDailyWorkout() + " and " + coach.getDailyFortune());
         //close application context
         context.close();
     }
